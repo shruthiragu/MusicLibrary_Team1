608541e0a590d9e965366dff00faf29b4e6c21f2
shruthiragu <115894437+shruthiragu@users.noreply.github.com>
2022-10-26 17:21:37 -0700

    Music Library Insert Page UI - Basic Version

--- MusicLibrary_Team1/MainPage.xaml.cs
+++ MusicLibrary_Team1/MainPage.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Microsoft.Win32;
+using System.Windows;
+using Windows.Storage.Pickers;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +18,28 @@ namespace MusicLibrary_Team1
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void CategoryTextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            var picker = new FileOpenPicker();
+            
         }
     }
 }
