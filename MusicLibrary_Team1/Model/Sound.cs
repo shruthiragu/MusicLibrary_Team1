97bf229ebb702afbfabfb2f36f85ba7824cc684f
heatheryi <heather.yi.work@gmail.com>
2022-10-28 12:58:56 -0400

    sound. cs

--- MusicLibrary_Team1/Model/Sound.cs
+++ MusicLibrary_Team1/Model/Sound.cs
@@ -3,32 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Perception.People;
 
 namespace MusicLibrary_Team1.Model
-{
+{ 
     internal enum PlaylistCategory
-    {
+{
         Chill,
         Classics,
         Dance,
         Upbeat
-
     }
-    internal class Playlist
+
+
+    internal class Sound
     {
-        public string TrackName { get; set; }   
-        public string ArtistName { get; set; }
-        Boolean Recommendable { get; set; } = true;
+        public string TrackName { get; set; }
+        public string AudioFile { get; set; }
+        public string ImageFile { get; set; }
         public PlaylistCategory Category { get; set; }
-
-        public Playlist(string name, PlaylistCategory category)
+    
+        public Sound(string name, PlaylistCategory category)
         {
-            TrackName = name;
-            Category = category;
-            Recommendable = false;  
 
+        }
 
         }
     }
-}
